dd272bd57a6247041a303c8fcb12d796b9147366
Ben Schmidt <bens@polyswarm.io>
2020-05-06 19:21:16 -0400

    add communities

--- src/polyd_events/__init__.py
+++ src/polyd_events/__init__.py
@@ -1,3 +1,8 @@
 from . import events, producer, consumer
 import inflection as _inflection
 event_types = [_inflection.underscore(cls.__name__) for cls in events.Event.__subclasses__()]
+
+communities = [
+    'nu',
+    'lima'
+]
