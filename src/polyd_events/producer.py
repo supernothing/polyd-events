4d306b61cebc5618907888de69afbfb18b4c1351
Ben Schmidt <bens@polyswarm.io>
2020-05-07 15:57:25 -0400

    re-work file_downloaded serialization

--- src/polyd_events/producer.py
+++ src/polyd_events/producer.py
@@ -1,6 +1,5 @@
-
 class EventProducer(object):
-    def __init__(self, stream, db):
+    def __init__(self, stream, db, max_len=None):
         """
         An event producer
 
@@ -9,6 +8,8 @@ class EventProducer(object):
         """
         self.db = db
         self.stream = self.db.Stream(stream)
+        self.max_len = max_len
 
     def add_event(self, event):
-        self.stream.add({'event': event.serialize()})
+        self.stream.add()
+        self.stream.add({'event': event.serialize()}, maxlen=self.max_len)
