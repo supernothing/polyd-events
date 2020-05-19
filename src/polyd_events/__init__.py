527dbd4c538253bed9a4096711e1ba4ee32202bf
Ben Schmidt <bens@polyswarm.io>
2020-05-19 18:00:14 -0400

    remove nu

--- src/polyd_events/__init__.py
+++ src/polyd_events/__init__.py
@@ -3,7 +3,6 @@ import inflection as _inflection
 event_types = [_inflection.underscore(cls.__name__) for cls in events.Event.__subclasses__()]
 
 communities = [
-    'nu',
     'omicron',
     'rho',
 ]
